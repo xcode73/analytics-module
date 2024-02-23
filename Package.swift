51d2c4aa7f09545cf8786ccff1ed84660f3ecba4
xcode73 <xcode.uln@gmail.com>
2024-02-23 17:19:22 +0400

    swift-tools-version:5.9

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
@@ -10,10 +10,10 @@ let package = Package(
         .library(name: "AnalyticsModule", targets: ["AnalyticsModule"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/xcode73/feather-core", .branch("test-dev")),
-        .package(url: "https://github.com/xcode73/analytics-objects", .branch("test-dev")),
+        .package(url: "https://github.com/xcode73/feather-core", branch: "test-dev"),
+        .package(url: "https://github.com/xcode73/analytics-objects", branch: "test-dev"),
         .package(url: "https://github.com/malcommac/UAParserSwift", from: "1.2.1"),
-        .package(name: "ALanguageParser", url: "https://github.com/matsoftware/accept-language-parser", from: "1.0.0"),
+        .package(url: "https://github.com/matsoftware/accept-language-parser", from: "1.0.0"),
         .package(url: "https://github.com/vapor/sql-kit.git", from: "3.28.0"),
     ],
     targets: [
@@ -21,11 +21,12 @@ let package = Package(
             .product(name: "Feather", package: "feather-core"),
             .product(name: "AnalyticsObjects", package: "analytics-objects"),
             .product(name: "UAParserSwift", package: "UAParserSwift"),
-            .product(name: "ALanguageParser", package: "ALanguageParser"),
+            .product(name: "ALanguageParser", package: "accept-language-parser"),
             .product(name: "SQLKit", package: "sql-kit"),
         ],
         resources: [
             .copy("Bundle"),
         ]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
